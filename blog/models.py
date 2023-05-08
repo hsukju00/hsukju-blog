a88accc4e08a6b0923b92ae4e21d68fe0ca9ebc7
hsukju <ehensnfl@gmail.com>
2023-05-08 23:46:21 +0900

    create category, post, tag models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=20, verbose_name="카테고리")
+    upper_category = models.ForeignKey("self", null=True)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50, verbose_name="제목")
+    content = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    published_at = models.DateField(auto_now_add=True)
+    modified_at = models.DateField(null=True, blank=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20, verbose_name="태그")
+    posts = models.ManyToManyField(Post)
