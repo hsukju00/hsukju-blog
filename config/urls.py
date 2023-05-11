cc54cad63aa067b918c048ffec050a953a3d09b3
hsukju00 <ehensnfl@gmail.com>
2023-05-11 22:20:55 +0900

    adjust project

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for hsukju-blog project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -17,4 +17,4 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("blog/", include("blog.urls"))]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("blog.urls"))]
--- config/wsgi.py
+++ config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hsukju-blog project.
+WSGI config for config project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
