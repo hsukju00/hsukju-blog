b1ae0879b81240d367ae1f2a168b6c0bcd05d046
hsukju00 <ehensnfl@gmail.com>
2023-05-12 04:15:36 +0900

    fix: not recognizing dotenv file properly

--- blogproject/config/settings.py
+++ blogproject/config/settings.py
@@ -14,8 +14,8 @@ import dotenv
 import os
 from pathlib import Path
 
-DOTENV_FILE = dotenv.find_dotenv()
-dotenv.load_dotenv(DOTENV_FILE)
+DOTENV_PATH = "../.env"
+dotenv.load_dotenv(dotenv_path=DOTENV_PATH)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
