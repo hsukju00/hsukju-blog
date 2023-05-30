cf06cd31169f59d198a42448140dbb56b2e26d3c
hsukju00 <ehensnfl@gmail.com>
2023-05-30 19:11:01 +0900

    create post_detail page

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,20 +1,17 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator
 
 from .models import Post
 
 
 def index(request):
-    posts = Post.objects.order_by('-published_at')
-    paginator = Paginator(posts, 5)
-    posts_in_page = paginator.get_page(1)
-    context = {"posts": posts_in_page}
+    post_list = Post.objects.order_by('-published_at')[:5]
+    context = {"post_list": post_list}
     return render(request, "blog/main.html", context)
 
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    context = {post: post}
+    context = {"post": post}
     return render(request, "blog/post_detail.html", context)
 
 
